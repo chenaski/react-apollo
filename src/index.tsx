d493a4779c96f538eb248409341ae30333ad8037
chenaski <mail2chenaski@gmail.com>
2021-04-29 22:32:39 +0300

    Init

---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { gql } from "@apollo/client/core";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        books {
+          title
+          author
+        }
+      }
+    `,
+  })
+  .then(console.log);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
